4229f955bbafee323101d7d3439a9e88ec7a9987
Nathan Russell <russellnathanielr@gmail.com>
2021-10-30 18:29:53 -0400

    starting procedural generation

---
+++ Assets/Procedural Generation/PlatformGeneration.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformGeneration : MonoBehaviour
+{
+    [SerializeField] int width, height;
+    [SerializeField] int minHeight, maxHeight;
+    [SerializeField] int repeatNum;
+    [SerializeField] GameObject lunarSoil, lunarCrust;
+
+    void Start() {
+        Generation();
+    }
+
+    void Generation() {
+        int repeatValue = 0;
+        for(int x=0;x<width;x++) {
+            if(repeatValue == 0) {
+                height = Random.Range(minHeight, maxHeight);
+                GenerateFlatPlatform(x);
+                repeatValue = repeatNum;
+            } else {
+              GenerateFlatPlatform(x);
+              repeatValue--;  
+            }
+        }
+    }
+
+    void GenerateFlatPlatform(int x) {
+        for(int y=0;y<height;y++) {
+            spawnObject(lunarSoil, x, y);
+        }
+        spawnObject(lunarCrust, x, height);
+    }
+
+    void spawnObject(GameObject obj, int width, int height) {
+        obj = Instantiate(obj, new Vector2(width, height), Quaternion.identity);
+        obj.transform.parent = this.transform;
+    }
+}
---
+++ Assets/Procedural Generation/ProceduralGeneration.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProceduralGeneration : MonoBehaviour
+{
+    [SerializeField] int width, height;
+    [SerializeField] int minRockHeight, maxRockHeight;
+    [SerializeField] GameObject lunarSoil, lunarCrust, lunarRock;
+
+    void Start() {
+        Generation();
+    }
+
+    void Generation() {
+        for(int x=0;x<width;x++) {
+            int minHeight = height - 1;
+            int maxHeight = height + 2;
+
+            height = Random.Range(minHeight, maxHeight);
+            int minRockSpawnDistance = height - minRockHeight;
+            int maxRockSpawnDistance = height - maxRockHeight;
+            int totalRockSpawnDistance = Random.Range(minRockSpawnDistance, maxRockSpawnDistance);
+            for(int y=0;y<height;y++) {
+                if(y < totalRockSpawnDistance) {
+                    spawnObject(lunarRock, x, y);
+                } else {
+                    spawnObject(lunarSoil, x, y);
+                }
+            }
+            if(totalRockSpawnDistance == height) {
+                spawnObject(lunarRock, x, height);
+            } else {
+                spawnObject(lunarCrust, x, height);
+            }
+        }
+    }
+
+    void spawnObject(GameObject obj, int width, int height) {
+        obj = Instantiate(obj, new Vector2(width, height), Quaternion.identity);
+        obj.transform.parent = this.transform;
+    }
+}
