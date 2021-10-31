5a1895c2b1133aabcd3bb4ea1cd9359b39e49a91
Nathan Russell <russellnathanielr@gmail.com>
2021-10-31 00:39:13 -0400

    more cave work, adding temporary textures, and 'tree' functionality

--- Assets/Procedural Generation/TerrainGenerator.cs
+++ Assets/Procedural Generation/TerrainGenerator.cs
@@ -5,15 +5,26 @@ using UnityEngine.Tilemaps;
 
 public class TerrainGenerator : MonoBehaviour
 {
-    public Sprite tile;
-    public float terrainAmount;
+    public int lunarSoilLayer = 5;
+    public Sprite log;
+    public Sprite leaf;
+    public Sprite lunarCrust;
+    public Sprite lunarSoil;
+    public Sprite lunarRock;
+
+    public int treeChance = 10;
+    public bool generateCaves = true;
+    public float terrainAmount = 0.25f;
     public int worldSize = 100;
     public float caveFreq = 0.05f;
     public float terrainFreq = 0.05f;
     public float heightMultiplier = 4f;
     public int heightAddition = 25;
+
     public float seed;
     public Texture2D noiseTexture;
+
+    public List<GameObject> worldTiles = new List<GameObject>();
 
     private void Start() {
         seed = Random.Range(-1000000, 1000000);
@@ -25,12 +36,25 @@ public class TerrainGenerator : MonoBehaviour
         for(int x=0;x<worldSize;x++) {
             float height = Mathf.PerlinNoise((x + seed) * terrainFreq, seed * terrainFreq) * heightMultiplier + heightAddition;
             for(int y=0;y<height;y++) {
-                if(noiseTexture.GetPixel(x, y).r > 0.25f) {
-                    GameObject newTile = new GameObject(name = "tile");
-                    newTile.transform.parent = this.transform;
-                    newTile.AddComponent<SpriteRenderer>();
-                    newTile.GetComponent<SpriteRenderer>().sprite = tile;
-                    newTile.transform.position = new Vector2(x + 0.5f, y + 0.5f);
+                Sprite tileSprite;
+                if(y < height - lunarSoilLayer) {
+                    tileSprite = lunarRock;
+                } else if(y < height - 1) {
+                    tileSprite = lunarSoil;
+                } else {
+                    tileSprite = lunarCrust;
+
+                    int t = Random.Range(0, treeChance);
+                    if(t == 1) {
+                        GenerateTree(x, y + 1);
+                    }
+                }
+                if(generateCaves) {
+                    if(noiseTexture.GetPixel(x, y).r > terrainAmount) {
+                        PlaceTile(tileSprite, x, y);
+                    }
+                } else {
+                    PlaceTile(tileSprite, x, y);
                 }
             }
         }
@@ -46,5 +70,20 @@ public class TerrainGenerator : MonoBehaviour
         }
 
         noiseTexture.Apply();
+    }
+
+    void GenerateTree(float x, float y) {
+        PlaceTile(log, x, y);
+    }
+
+    public void PlaceTile(Sprite tileSprite, float x, float y) {
+        GameObject newTile = new GameObject();
+        newTile.transform.parent = this.transform;
+        newTile.AddComponent<SpriteRenderer>();
+        newTile.GetComponent<SpriteRenderer>().sprite = tileSprite;
+        newTile.name = tileSprite.name;
+        newTile.transform.position = new Vector2(x + 0.5f, y + 0.5f);
+
+        worldTiles.Add(newTile);
     }
 }
